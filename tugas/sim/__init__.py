3be6a1e50ddd959c7e0d17f9ed1580c8a725c0ab
Miummiumi <ummikalsumsaleh@gmail.com>
2021-01-07 22:38:41 +0900

    first commit

---
+++ tugas/sim/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app=Flask('__name__', template_folder='sim/templates', static_folder='sim/static')
+app.config['SECRET_KEY']='qwerty'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sim.pengaduan.db'
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_menager=LoginManager(app)
+
+
+from sim.mahasiswa.routes import rmahasiswa
+app.register_blueprint(rmahasiswa)
